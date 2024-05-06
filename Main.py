09fb357f6e14f9458bf4176a62b13bb7feb6be0d
MDeGray0 <167363151+MDeGray0@users.noreply.github.com>
2024-05-06 12:30:23 -0400

    Cleaned_up
    Should be cleaned up. Also should be 30 out of 30

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ import pygame #importing lobby
 import os #importing oppertating system
 import random
 
+#Creating the board
 class Box: 
     def __init__(self, x, y, blocksize, is_bomb) -> None:
         self.x = x
@@ -22,11 +23,7 @@ class Box:
 
     def __repr__(self):
         return f'Box: row{self.row}, col:{self.col}'
-
-    # def draw_flag(self, window, flag_image):
-    #     if self.is_flagged: 
-    #         window.blit(flag_image, self.rect.topleft)
-            
+          
 # Initialize Pygame
 pygame.init()
 pygame.font.init()
@@ -35,8 +32,8 @@ clock = pygame.time.Clock()
 #window size
 width = 800
 height = 800
-window = pygame.display.set_mode((width, height)) #just displaying obviously. Don't neccessarily need width & height but more for organization.
-pygame.display.set_caption("Mine Sweeper") #Top bar (like where x, minimize, full window go)
+window = pygame.display.set_mode((width, height)) 
+pygame.display.set_caption("Mine Sweeper") 
 
 #colors
 white = (200, 200, 200)
@@ -45,27 +42,25 @@ blue = (0, 0, 128)
 red = (255, 0, 0) 
 black = (0, 0, 0)
 
-# font
+# font & directions
 font = pygame.font.Font('freesansbold.ttf', 32)
 smallfont = pygame.font.Font('freesansbold.ttf', 20)
-#text = font.render('1', True, green, blue)
 directionsline1 = smallfont.render('This is a traditional minesweeper game.', True, white, black)
 directionsline2 = smallfont.render('10 bombs are hidden within the gameboard', True, white, black)
 directionsline3 = smallfont.render("Left click to clear the box. Right click to mark a flag. 'Q' to quit.", True, white, black)
 directionsline4 = smallfont.render("Don't hit the bombs!", True, white, black)
 
-#directions
-
 #creating the bombs
-BOMB_COUNT = (10,10)
-blocksize = 50  #Reads each of the rectanges as individauls 
+BOMB_COUNT = (30,30)
+blocksize = 50  
 running = True 
 winning = False
 gameover = False
-BOXES = [] #creating a list of boxes. Also keeps the boxes as individuals.
+BOXES = [] 
 FLAGS = BOMB_COUNT[0]
 FLAGS2 = FLAGS
 
+#Finding the boxes
 def find_all(r, c):
     direction = [(-1,-1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)]
     used = []
@@ -79,38 +74,36 @@ def find_all(r, c):
         row = box.row
         col = box.col
         used.append(box)
-
-        for item in direction: #stack adds things on top. 
+        for item in direction:  
             if 0 <= row + item[0] < len(BOXES) and 0 <= col + item[1] < len(BOXES):
                 if BOXES[row + item[0]][col + item[1]].bombs == 0 and BOXES[row + item[0]][col + item[1]].is_bomb == False:
                     if (BOXES[row + item[0]][col + item[1]] not in used and BOXES[row + item[0]][col + item[1]] not in stack):
                         stack.append(BOXES[row + item[0]][col + item[1]])
-                    
                 elif BOXES[row + item[0]][col + item[1]].bombs != 0 and BOXES[row + item[0]][col + item[1]].is_bomb == False:
                     BOXES[row + item[0]][col + item[1]].open = True
                     BOXES[row + item[0]][col + item[1]].color = (220,220,220)
-            #checks the neighbors 
-
+          
+#Customizing the boxes more
 row_index = 0
-for y in range(100, height - 100, blocksize): #100 and -100 just adjust where the blocks are
+for y in range(100, height - 100, blocksize): 
     row = []
     col_index = 0
     for x in range(100, width - 100, blocksize):
         new_box = Box(x, y, blocksize, False)
         new_box.set_row_col(row_index, col_index)
-        row.append(new_box) #referring back the lists. 
-        #this makes the boxes before the loops. 
+        row.append(new_box) 
         col_index += 1
     BOXES.append(row)
     row_index += 1
 
-for bomb in range(random.randrange(BOMB_COUNT[0], BOMB_COUNT[1] + 1)): #makes sure the bombs are within the grid itself. 
+#Bomb creation
+for bomb in range(random.randrange(BOMB_COUNT[0], BOMB_COUNT[1] + 1)):
     bomb_row = random.randrange(0,len(BOXES))
     bomb_col = random.randrange(0,len(BOXES))
-    #BOXES[bomb_row][bomb_col].color = red #will currently show where bombs are as red. 
     BOXES[bomb_row][bomb_col].is_bomb = True 
 
-direction = [(-1,-1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)] #checking to see how close bomb is (so basically 1 away). If there is touching more than 1, it is noted. 
+#Checks for the bombs in the boxes next to it. 
+direction = [(-1,-1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)]  
 for row in range(len(BOXES)):
     for col in range(len(BOXES[row])):
         counter = 0
@@ -118,30 +111,31 @@ for row in range(len(BOXES)):
             if 0 <= row + item[0] < len(BOXES) and 0 <= col + item[1] < len(BOXES):
                 if BOXES[row + item[0]][col + item[1]].is_bomb == True:
                     counter += 1
-        BOXES[row][col].bombs = counter #checking for the bombs
+        BOXES[row][col].bombs = counter 
 
-flag_image = pygame.image.load(os.path.join('Assets', 'flag.png'))
+#Flag information
+flag_image = pygame.image.load(os.path.join('CSCI Final','Assets', 'flag.png'))
 flag = pygame.transform.scale(flag_image, (40, 40))
 flag_dest = (blocksize//2, blocksize//2)
 flag_x = 100
 flag_y = 50
 flag_font = pygame.font.Font('freesansbold.ttf', 20)
 
-
+#Win screen 
 def checkWin():
     for row in BOXES:
         for box in row:
-            if box.open == True or box.is_bomb ==True:
+            if box.open == True or box.is_bomb == True:
                 continue
             else:
                 return False
             
     return True
 
-
+#Game initiation
 while running: 
     window.fill((0, 0, 0)) 
-    flag_score = font.render(f'Flags remaining: {FLAGS} /{FLAGS2}', True, white)
+    flag_score = font.render(f'Flags remaining: {FLAGS} / {FLAGS2}', True, white)
     window.blit (flag_score, (flag_x, flag_y))
     window.blit(directionsline1, (100, 707))
     window.blit(directionsline2, (100, 727))
@@ -156,7 +150,7 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN: 
             if event.key == pygame.K_q: 
-                running = False #press q to quit. 
+                running = False 
         elif event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
             clicked_box = None
@@ -164,14 +158,12 @@ while running:
                 for box in row:
                     if box.rect.collidepoint(pos):
                         clicked_box = box
-            if clicked_box and event.button == 1: #makes sure nothing happens if an area that isn't a boxed is clicked it doesn't do anything. Also fills box.
+            if clicked_box and event.button == 1: 
                 if clicked_box.is_bomb:  
                     running = False
-                    gameover = True #hit bomb, game ends
-                    #print("You're dead") 
+                    gameover = True 
                     clicked_box.color = red
                 else: 
-                    # clicked_box.color = green
                     find_all(clicked_box.row, clicked_box.col)
                     print(pos, clicked_box.bombs)  
                     print(clicked_box.row, clicked_box.col)
@@ -180,28 +172,23 @@ while running:
                         if FLAGS > 0:
                             clicked_box.is_flagged = True
                             FLAGS -= 1
-                        # clicked_box.draw_flag(window, flag)
                     else: 
                         clicked_box.is_flagged = False 
                         FLAGS += 1
-                    #     box.draw_flag(window, white, box.rect)
-                    #     pygame.display.update(box.rect)
-                    print(FLAGS)
 
-
+#Box and flag check
     for row in BOXES:
         for box in row:                
-    
             if box.color == white:
                 pygame.draw.rect(window, box.color, box.rect, 1)
-                 #this helps store each of the boxes. Rerenders the boxes in the boxes list.
             else: 
-                pygame.draw.rect(window, box.color, box.rect) #referring back to elif event.
-                pygame.draw.rect(window, white, box.rect,1) #referring back to elif event.
+                pygame.draw.rect(window, box.color, box.rect) 
+                pygame.draw.rect(window, white, box.rect,1) 
                 if box.bombs != 0 and box.is_bomb == False:
                     w, h = font.size(str(box.bombs))
                     text = font.render(str(box.bombs),False, (105, 105, 105))
                     window.blit(text, (box.x + (50 - w) // 2, box.y + (50 - h) //2 ))
+
             if box.is_flagged == True:
                 pygame.draw.rect(window, box.color, box.rect, 1) == False
                 pygame.draw.rect(window, box.color, box.rect) == False
@@ -241,7 +228,7 @@ while winning == True:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q: 
                 running = False 
-                winning = False #press q to quit. 
+                winning = False 
     window.fill((0,0,0))
     window.blit(winText, (305, 365))
     pygame.display.update()
